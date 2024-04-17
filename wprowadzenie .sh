c019620c3e28f2bb2eb96c8d4946e57c3def091b
jasio56 <jasiekdomagala04@gmail.com>
2024-04-17 14:49:30 +0200

    First commit

---
+++ wprowadzenie .sh
@@ -0,0 +1,72 @@
+git commit
+git commit
+
+git branch bugFix 
+git checkout bugFix
+
+git commit   
+git checkout main 
+git commit 
+git merge bugfix
+
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git checkout BugFix
+git rebase main
+
+ "" kliknąc na C4 ""
+
+git branch -f master C6
+git branch -f bugFix C0
+git checkout C1
+
+
+
+ 4 
+git checkout pushed
+ git revert HEAD
+ git  checkout local
+ git reset HEAD~1
+
+
+
+ git cherry-pick C3 C4 C7
+
+ git rebase -i HEAD~4
+
+git checkout main
+git cherry-pick C4
+
+git rebase -i caption~2 --aboveAll
+git commit --amend
+git rebase -i caption~2 --aboveAll
+git branch -f main caption
+
+git checkout master
+git cherry-pick C2
+git commit --amend
+git cherry-pick C3
+ 
+git tag v0 C1
+git tag v1 C2
+git checkout C2
+
+git commit
+
+
+git rebase main bugFix
+git rebase bugFix side
+git rebase side another
+git rebase another main
+
+
+git branch bugWork main~^2~
+
+git checkout one
+git cherry-pick C4 C3 C2
+git checkout two
+git cherry-pick C5 C4 C3 C2
+git branch -f three C2
